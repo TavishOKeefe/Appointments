f1d7dacfbf76e327d2bbdc5fb3214338376439d7
Scott Bergler and Tavish OKeefe <scott+tavish@>
2018-10-17 16:38:04 -0700

    add survey after submission.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   $("form#apptForm").submit(function(event) {
     event.preventDefault();
 
-      // FRONT END
     var firstName = $("#first-name").val();
     var lastName = $("#last-name").val();
     var purpose = $("#reason").val();
@@ -11,28 +10,17 @@ $(document).ready(function() {
     var startTime = $("#start").val();
     var endTime = $("#end").val();
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(purpose);
-    console.log(apptDay);
-    console.log(startTime);
-    console.log(endTime);
-
-    // BACKEND
-    $("#success").text("Your appointment is set! Here are the details:");
     $("#name").text(firstName + " " + lastName);
     $("#purpose").text(purpose);
     $("#dateTime").text(apptDay + " " + startTime + " " + endTime);
 
-    // $("#name").text(firstName + lastName);
-    // $("#purpose").text(purpose);
-    // $("#dateTime").text(apptDay + startTime + endTime);
+    $("#apptForm").addClass("hide");
+    $("#welcome").addClass("hide");
+    $("#apptbtn").addClass("hide");
+    $("#surveybtn").removeClass("hide");
+    $("#survey").removeClass("hide");
+    $("#booked").removeClass("hide");
 
   });
-
-
-
-
-
 
 });
